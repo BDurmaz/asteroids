9433fef4f3e8ef944c1eb148eb63c1a495b76ec8
BDurmaz <my_wall_2010@hotmail.com>
2025-03-01 23:35:53 +0100

    Add player movement and container groups for easy management

--- main.py
+++ main.py
@@ -7,23 +7,32 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
+            
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit fps to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
