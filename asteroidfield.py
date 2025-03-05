f21ecc76b8f78eb5694c55500b62e42a9c9e7b93
BDurmaz <my_wall_2010@hotmail.com>
2025-03-05 14:30:55 +0100

    fix issues in code and add game over text

--- asteroidfield.py
+++ asteroidfield.py
@@ -1,8 +1,8 @@
 import pygame
 import random
-
 from asteroid import Asteroid
 from constants import *
+
 
 class AsteroidField(pygame.sprite.Sprite):
     edges = [
@@ -14,7 +14,7 @@ class AsteroidField(pygame.sprite.Sprite):
             pygame.Vector2(-1, 0),
             lambda y: pygame.Vector2(
                 SCREEN_WIDTH + ASTEROID_MAX_RADIUS, y * SCREEN_HEIGHT
-            )
+            ),
         ],
         [
             pygame.Vector2(0, 1),
@@ -36,12 +36,12 @@ class AsteroidField(pygame.sprite.Sprite):
         asteroid = Asteroid(position.x, position.y, radius)
         asteroid.velocity = velocity
 
-    def update(self,dt):
+    def update(self, dt):
         self.spawn_timer += dt
         if self.spawn_timer > ASTEROID_SPAWN_RATE:
             self.spawn_timer = 0
 
-            #spawn a new asteroid at a random edge
+            # spawn a new asteroid at a random edge
             edge = random.choice(self.edges)
             speed = random.randint(40, 100)
             velocity = edge[0] * speed
